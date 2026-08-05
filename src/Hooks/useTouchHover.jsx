df98292bc0b9c4d96e99133cee5c1af63b2bc3e7
Aman kureshi <amankureshi78692@gmail.com>
2026-08-05 22:23:05 +0530

    addes custom hooks

--- src/Hooks/useTouchHover.jsx
+++ src/Hooks/useTouchHover.jsx
@@ -4,9 +4,17 @@ export default function useTouchHover() {
     const [active, setActive] = useState(false);
 
     const touchProps = {
-        onTouchStart: () => setActive(true),
-        onTouchEnd: () => setActive(false),
+        onTouchStart: () => {
+            setActive(true);
+        },
+
+        onTouchEnd: () => {
+            setActive(false);
+        },
     };
 
-    return { active, touchProps };
+    return {
+        active,
+        touchProps,
+    };
 }
